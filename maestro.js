793f374861f052e11a09b5d8b9ef7cf61608b79a
lucasolivier <lucas.o@live.com>
2015-04-26 13:52:15 -0300

    Suit current code to MaestroSocket

--- maestro.js
+++ maestro.js
@@ -32,6 +32,7 @@ var express = require("express");   //get express module
 var http  = require("http");    //get http module
 var cfenv = require("cfenv");   //get cloud foundry enviroment module
 var ws = require("ws"); //get the websocket module
+var MaestroSocket = require("./MaestroSocket");  //Import MaestroSocket class from MaestroSocket.js
 
 var app = express();    //inits the express application
 
@@ -47,241 +48,144 @@ httpServer.listen(appEnv.port, function() {
     console.log("Maestro starting on " + appEnv.url)
 });
 
+app.get("/", function(req, res) {   //respond the request for server/maestrostatus
+    res.send("OK"); //for while respond with status word       
+});
+
 app.get("/status", function(req, res) {   //respond the request for server/maestrostatus
     res.send("status"); //for while respond with status word       
 });
 
 var wsServer = new ws.Server({ server: httpServer });   //'promotes' the httpserver to a websocket server
 
-var MsgHandler = new Object();  //object to hold event creations
-
 wsServer.on("error", function(error) {  //instance to handle websocket server errors
-    console.log("Error while connecting websocket: " + error);    
+    console.log("Error while operating WebSocketServer: " + error);    
 });
+
+
+//--------------------------------------- SETUP MADE, NOW WHAT REALLY MATTERS :) -----------------------------------------------
+
+
+var connections = [];   //array to store connection objects
+
+var sessions = [];  //array to store session objects
 
 wsServer.on("connection", function(wSocket) {   //websocket connection event handler
     
-    wSocket.on("error", function(error) {   //instance to handle websocket errors
-        log("Error while dealing new websocket: " + error);       
-    });
-
-    wSocket.on("close", function(code, message) {   //instance to handle websocket errors
-        log("Closing websocket: " + code + " " + message);         
-    });
-
-    wSocket.on("message", function(message) {   //instance to handle websocket messages
-        
-        
-    });
-});
-
-
-function MaestroSocket(wSocket) {   //MaestroSocket class to handle websocket information
+    var client = new MaestroSocket(wSocket);
     
-    var self = this;    //holds its own ref
-    
-    var events = [];    //events array to store callbacks
-    
-    wSocket.onerror = function(error) {
-        if(events["error"])
-            events["error"].call(this, error);       
-    };    
-    
-    wSocket.onclose = function(code, message) {
-        if(events["close"])
-            for(cbIndex in events["close"])   //for each callback in the event array,
-                events["close"][cbIndex].call(this, code, message); //fire with the args and its scope as "this" value    
-    };
-    
-    wSocket.onmessage = function(message, flags) {
-        var dataObj = getDataObj(message.data);  //get the data obj from the data message received    
-        
-        if(!dataObj.event || dataObj.event == "close" || dataObj.event == "error" || !events[dataObj.event])    
-            //check whether the dataObj.event is not present, if any of them are protected  and if there is not callback sign with that value
-            return; //if so, return
-        
-        for(cbIndex in events[dataObj.event])   //for each callback in the event array,
-            events[dataObj.event][cbIndex].apply(this, dataObj.args); //fire with the args and its scope as "this" value        
-    };
-    
-    this.on = function(event, callback) {   //sign callback event
-        if(!events[event])  //if the event specified is still empty
-            events[event] = []; // Inits the event name           
-        events[event].push(callback);   //push the callback to the array        
-    };
-
-    this.clear = function(event, position) {
-        if(!events[event])  //if the event is not signed, return
-            return;
-        if(!position)   //if the position is not specified, clear all
-            events[event] = null;
-        else
-            events[event][position] = null; //clear the event callback position       
-    };
-    
-    this.emit = function(event) {
-        try {    
-            //check true need for this verification due to if it disconnects it will be notifie
-            if(client.readyState != client.OPEN) //check if the socket is opened
-                throw "emitFailedSocketNotOpen";  //if not, throw an error socket not open       
-            var args = [].slice.call(arguments) // slice without parameters copies all
-            var dataObj = { event: args.shift(), args: args };  //create the data object with the data passed           
-            wSocket.send(getDataStr(dataObj));    //send the data string generated from the the dataobj
-        }
-        catch(error) {
-            if(events["error"])
-                events["error"].call(this, error);   
-        }       
-    }; 
-    
-    this.close = function() {   //must verify what else is needed to close the connection
-        //and verify if once this method is called, the onclose method is automatically called aswell or we need to force its call
-        socket.close();     //close the socket connection 
-    };
-    
-    function getDataStr(dataObj) {
-        return JSON.stringify(dataObj);
-    }
-
-    function getDataObj(dataStr) {
-        return JSON.parse(dataStr);
-    }
-}
-
-
-
-
-//var connections = [];
-
-//var sessions = [];
-
-
-server.on("connection", function(socket) {
-        
     var connId = "";
     var username = "";
     
     log("New connection.");
     
-    socket.on("close", function() {
+    client.on("error", function(error) {   //instance to handle websocket errors
+        log("Error while dealing new websocket: " + error);       
+    });
+
+    client.on("close", function(code, message) {   //instance to handle websocket errors
         //verify if an object has been created for this instance
         //if so, remove it;
         
         //got to verify what else should be discarted
         if(!connId)
-            log("Temp client disconnected.");
+            log("Temp client disconnected.");        
+    });
+
+    client.on("createSession", function() {
+        var newSession = createSession();
+        log("New session created: " + newSession);
+        log(lengthOf(connections) + " " + lengthOf(sessions));
+        client.emit("sessionCreated", newSession);
+    });
+               
+    client.on("joinSession", function(sessionId, user) {
+        //atribuite id only when joining sessions
+        //generate timeout in case information is not send by connected client
+
+        if(!sessions[sessionId]) {           
+            client.emit("joinError", sessionId);
+            log("Session join error");
+            client.close();
+            return;   
+        }
+        
+        //get session ref
+        connSession = sessionId;
+        username = user;
+        /*  Create new connection ID    */
+        connId = getId(10);    
+        while(connections[connId])
+        connId = getId(10);        
+        connections[connId] = socket;
+        log("New client ID: " + username + " " + connId);
+        /*  -------------------------   */
+        
+        /*          Join Session        */
+        sessions[sessionId].members[connId] = socket;
+        
+        if(sessions[sessionId].hostId == "") {
+            sessions[sessionId].hostId = connId;
+            socket.send(getDataStr({type: "sessionJoined",data: ["", sessionId, connId]}));
+        } else {
+            var hostId = sessions[sessionId].hostId;
+            socket.send(getDataStr({type: "sessionJoined",data: [hostId, sessionId, connId]}));
+                    //got to have two separate emites due to in case of host it has to be empty and it is changed in the middle way
+        }
+        
+        socket.on("close", function() {
+            
+            if(sessions[sessionId].members[connId])
+                delete sessions[sessionId].members[connId];
+
+            if(connections[connId])
+                delete connections[connId];
+            
+            log("Client " + connId + " disconnected.");
+            log("Session members: " + lengthOf(sessions[sessionId].members));
+            if(lengthOf(sessions[sessionId].members) == 0) {
+                log("Deleting empty session.");
+                delete sessions[sessionId];          
+            }
+            else if(sessions[sessionId].hostId == connId) {  //so you were the host, need to get a new
+            
+                log("Host has disconnected. Electing new host...");    
+                var newHostId = electHost(sessionId);  
+                log("ID: " + newHostId + " is now the new host.");
+            
+                log("Informing session members...");
+                log(lengthOf(sessions[sessionId].members));
+                for(memberId in sessions[sessionId].members) {
+                if(sessions[sessionId].hostId != memberId)
+                    sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [newHostId]}));
+                else
+                    sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [""]}));
+                }
+            
+                log("All sessions has been informed.");
+
+                log("Informing host socket is closed");
+                sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
+            } else {
+                log("Informing peers socket is closed");
+                sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
+            }
+        });
     });
     
-    socket.on("message", function(message) {
-        
-        var dataObj = getDataObj(message);
-        
-        switch(dataObj.type) {
-                
-            case "createSession":
-                var newSession = createSession();
-                log("New session created: " + newSession);
-                log(lengthOf(connections) + " " + lengthOf(sessions));
-                socket.send(getDataStr({type: "sessionCreated",data: [newSession]}));
-            break;
-                
-            case "joinSession":
-                //atribuite id only when joining sessions
-                //generate timeout in case information is not send by connected client
-                var sessionId = dataObj.data[0];
-                var user = dataObj.data[1];
-                
-                if(!sessions[sessionId]) {
-                    socket.send(getDataStr({type: "joinError", data: [sessionId]}));
-                    log("Session join error");
-                    socket.close();
-                    return;   
-                }
-        
-                //get session ref
-                connSession = sessionId;
-                username = user;
-                /*  Create new connection ID    */
-                connId = getId(10);    
-                while(connections[connId])
-                    connId = getId(10);        
-                connections[connId] = socket;
-                log("New client ID: " + username + " " + connId);
-                /*  -------------------------   */
-        
-                /*          Join Session        */
-                sessions[sessionId].members[connId] = socket;
-            
-                if(sessions[sessionId].hostId == "") {
-                    sessions[sessionId].hostId = connId;
-                    socket.send(getDataStr({type: "sessionJoined",data: ["", sessionId, connId]}));
-                } else {
-                    var hostId = sessions[sessionId].hostId;
-                    socket.send(getDataStr({type: "sessionJoined",data: [hostId, sessionId, connId]}));
-                    //got to have two separate emites due to in case of host it has to be empty and it is changed in the middle way
-                }
-        
-                socket.on("close", function() {
-            
-                    if(sessions[sessionId].members[connId])
-                        delete sessions[sessionId].members[connId];
-
-                    if(connections[connId])
-                        delete connections[connId];
-            
-                    log("Client " + connId + " disconnected.");
-                    log("Session members: " + lengthOf(sessions[sessionId].members));
-                    if(lengthOf(sessions[sessionId].members) == 0) {
-                        log("Deleting empty session.");
-                        delete sessions[sessionId];          
-                    }
-                    else if(sessions[sessionId].hostId == connId) {  //so you were the host, need to get a new
-            
-                        log("Host has disconnected. Electing new host...");    
-                        var newHostId = electHost(sessionId);  
-                        log("ID: " + newHostId + " is now the new host.");
-            
-                        log("Informing session members...");
-                        log(lengthOf(sessions[sessionId].members));
-                        for(memberId in sessions[sessionId].members) {
-                            if(sessions[sessionId].hostId != memberId)
-                                sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [newHostId]}));
-                            else
-                                sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [""]}));
-                        }
-            
-                        log("All sessions has been informed.");
-
-                        log("Informing host socket is closed");
-			             sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
-                    } else {
-                        log("Informing peers socket is closed");
-			             sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
-                    }
-                });
-        
-            break;
-                
-            case "peerData":
-            /*          SESSION JOINED              */
-            var destId = dataObj.data[0],
-                sessionId = dataObj.data[1],
-                data = dataObj.data[2];
-                
-            //maybe implement query id in the future
-                try {
-                    if(sessions[sessionId].members[destId])
-                        sessions[sessionId].members[destId].send(getDataStr({type: "peerData", data: [connId,data]}));
-                    else
-                        socket.send(getDataStr({type: "peerDataError", data: [destId,data]}));
-                } catch (error) {
+    client.on("peerData", function(destId, sessionId, data) {
+        //maybe implement query id in the future
+        try {
+            if(sessions[sessionId].members[destId])
+                sessions[sessionId].members[destId].send(getDataStr({type: "peerData", data: [connId,data]}));
+            else
+                socket.send(getDataStr({type: "peerDataError", data: [destId,data]}));
+        } catch (error) {
                     
-                }
-            break;
         }
     });
-    
 });
+
 
 
 
@@ -316,10 +220,6 @@ function electHost(sessionId) {
     
     
 }
-
-
-
-
 
 function lengthOf(obj) {
     var c=0;
