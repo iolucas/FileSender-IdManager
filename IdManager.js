190a3a043ca9015e7c4d4270ad7005b10d051396
lucasolivier <lucas.o@live.com>
2015-04-30 19:00:24 -0300

    Updated

--- IdManager.js
+++ IdManager.js
@@ -12,20 +12,39 @@ function IdManager() {
         mongoose = require("mongoose"), //get mongoose module
         
         //Connections = null, //Declare Connections Model
-        Sessions = mongoose.model("Sessions", { SessionId: String , HostId: String, Members: [] });   //Creates Session model
+        Sessions = mongoose.model("Sessions", { 
+            SessionId: String , 
+            HostId: { type: String, default: "" }, 
+            Members: { type: Array, default: [] }
+            //createdAt: { type: Date, expires: 1, default: Date.now }    //set the register expiration date
+        });   //Creates Session model
     
     //Function to Start IdManager and Invoke the callback passed 
     this.Start = function(dbUrl, callback) {
-        mongoose.connect(dbUrl, callback);    
+        mongoose.connect(dbUrl, function(error) {
+            
+            Sessions.remove(function(error){}); //clear Sessions DB
+            
+            //debug function for show sessions in the database
+            Sessions.find(function(findError, result) { 
+                console.log(result);                
+            });
+            callback(error);    //invoke the start callback method            
+        });    
     };
     
-    this.SessionClear = function() {
+    /*this.SessionClear = function() {
         Sessions.remove(function(error){});    
-    };
+    };*/
         
-    //Function to Get Session Handler
-    this.GetSessionId = function(id, callback) {      
-        Sessions.findOne({ SessionId: id }, callback);
+    //Function to Check whether a Session exists
+    this.CheckSessionId = function(id, callback) {      
+        Sessions.findOne({ SessionId: id }, function(error, sessionHandler) {
+            if(error || !sessionHandler)
+                callback(false);    //invoke callback with result false
+            else
+                callback(true); //invoke callback with result true           
+        });
     };
        
     //Function to Create New Session and Get its Handler
@@ -42,37 +61,54 @@ function IdManager() {
             } else {    //if no error and no handler is get, create new session entry, save it and invoke the finalCallback
                 var newSessionEntry = new Sessions({ SessionId: newSessionId });
                 newSessionEntry.save(function(error) {
-                    if(error)
+                    if(error)   //if error, log it, if not, invoke the finalCallback
                         console.log(error);
-                    else {
-                        Sessions.find(function(error, result) { //debug function for show sessions in the database
-                            console.log(result);
-                            finalCallback(newSessionId);
-                        });
-                    }
+                    else
+                        finalCallback(newSessionId);
                 });                
             }
         });
-    };  
+    };
+    
+    //Function to Join an existent Session
+    this.JoinSession = function(sessionId, connection, callback) {
+        Sessions.findOne({ SessionId: sessionId, }, function(error, session) {   //get session handler
+            //if session not found or error 
+            if(error || !session) {  
+                callback(false);    //invoke callback informing with one false arg
+                return; //do nothing else and return
+            }
+            
+            //---------------   Create New Connection ID    ------------------//
+                    
+            do {    //Keep getting alphanumeric id until find one not used   
+                var newConnId = GetAlphaNumId(20);                   
+            } while(session.Members[newConnId]);
+            
+            connection.ConnectionId = newConnId;  //gets client the new connectiond id                           
+            
+            //---------------   Join Session    ------------------//
+            session.Members[newConnId] = connection; //put the connection id at the session members
+            if(session.HostId == "") { //if there is no host,
+                session.HostId == connection.ConnectionId;  //set this joiner as host
+                callback("HOST");  //invoke the end callback with the OWNER msg
+            } else 
+                callback(session.HostId);  //invoke the end callback with the owner connID           
+        });  
+    };
+    
+    this.GetClientHandler = function(sessionId, clientId, callback) {
+        Sessions.findOne({ SessionId: sessionId, }, function(error, session) {
+            //if some error while getting the session or clientId not found,
+            if(error || !session || !session.Members[clientId]) {  
+                callback(false);    //invoke callback informing with one false arg
+            }  else  //if everithing goes good, 
+                callback(session.Members[clientId]);    //get the clientid handler
+        });  
+    };
 }
 
-function createSession() {
-    
-    do {
-        var newSessionId = getWordId();       
-    } while(sessions[newSessionId]);
-    
-    /*var newSessionId = getId(5);
-        
-    while(sessions[newSessionId])
-        newSessionId = getId(5);*/
-             
-    sessions[newSessionId] = new Object();
-    sessions[newSessionId].hostId= "";
-    sessions[newSessionId].members = [];
-    
-    return newSessionId;
-}
+
 
 function getWordId()
 {
@@ -84,7 +120,7 @@ function getWordId()
     if(Math.random() > 0.5)
         numberFirst = true;
     
-    var numberId = Misc.GetNumId(3);
+    var numberId = GetNumId(3);
     
     var wIndex = (Math.random() * wordList.length).toFixed(0);  //got to fix 0 decimal places to use an index
     var wordId = wordList[wIndex];  
@@ -96,3 +132,19 @@ function getWordId()
     else
         return wordId+numberId; 
 }
+
+function GetNumId(size) {   
+        var text = "";
+        var possible = "0123456789";
+        for( var i=0; i < size; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+}; 
+
+function GetAlphaNumId(size) {   
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+    for( var i=0; i < size; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+};
--- maestro.js
+++ maestro.js
@@ -16,6 +16,7 @@ var cfenv = require("cfenv"),   //get cloud foundry enviroment module
     wsServer = new ws.Server({ server: httpServer }), //'promotes' the httpserver to a websocket server
     idManager = new IdManager();  //inits new IdManager instance
 
+
 //------------------------------------------------------------------------------------//
 
 //SERVE HTTP FILES IN PUBLIC FOLDERS
@@ -33,11 +34,11 @@ app.get("/", function(req, res) {
 app.get("/*", function(req, res) {  
     //CHECK REQUESTED GET/*
     if(req.path.indexOf(".") == -1) //verify if the request has no dot, meaning that is session request
-        idManager.GetSessionId(req.path.substr(1), function(error, sessionHandler) {    //verify if the requested if exists
-            if(sessionHandler)  //if session handler is found,
+        idManager.CheckSessionId(req.path.substr(1), function(result) {    //verify if the requested if exists
+            if(result)  //if session handler is found,
                 res.sendFile(__dirname + "/public/session.html");   //respond session page
             else    //if not,
-                res.send("Not Found");  //respond with not found
+                res.send("Not Found");  //respond with error page
         });
     else    //if any dot is found, 
         res.end();  //finish the response
@@ -48,11 +49,111 @@ wsServer.on("error", function(error) {
     log("Error while operating WebSocketServer: " + error);    
 });
 
+//INSTANCE TO HANDLE CONNECTIONS IN WSSERVER
+wsServer.on("connection", function(wSocket) {
+    //once it is connected, set timeOut in case it do not connect to any session, be disconnected
+    
+    var client = new Wocket(wSocket);   //inits wocket instance with the just connected websocket
+    
+    client.ConnectionId = "";
+    client.SessionId = "";
+    client.Username = "";
+    
+    client.on("error", function(error) {   //instance to handle websocket errors
+        log("Error while dealing with websocket: " + client.Username+"@" + client.ConnectionId + " -> " + error);       
+    });
+    
+    client.on("JoinSession", function(sessionId, username) {
+        //generate timeout in case information is not send by connected client, disconnect it
+
+        if(client.Username != "") //if another request be made with a Username fullfilled, do nothing, return
+            return;
+        
+        client.Username = username;    //gets the sent username
+        
+        //Executes the JoinSession method
+        idManager.JoinSession(sessionId, client, function(hostResult) {
+            //IF HOST RESULT GOES BAD
+            if(!hostResult) {   
+                log("Session Join Error: " + sessionId);   //log error in the screen
+                client.emit("JoinError");    //emit joinError
+                client.close(); //close client connection
+                return;   //do nothing else, and return                     
+            }           
+            
+            //IF HOST RESULT GOES GOOD
+            
+            //SET CONNECTED CLIENT EVENTS
+            
+            client.on("PeerData", function(destId, data) {
+                
+                idManager.GetClientHandler(client.SessionId,destId, function(destHandler) {
+                //maybe implement query id in the future
+                    try {
+                        if(sessions[sessionId].members[destId])
+                            sessions[sessionId].members[destId].send(getDataStr({type: "peerData", data: [connId,data]}));
+                        else
+                            socket.send(getDataStr({type: "peerDataError", data: [destId,data]}));
+                    } catch (error) {}  
+                });              
+            });
+            
+            client.on("close", function() {
+            
+                if(sessions[sessionId].members[connId])
+                    delete sessions[sessionId].members[connId];
+
+                if(connections[connId])
+                    delete connections[connId];
+            
+                log("Client " + connId + " disconnected.");
+                log("Session members: " + lengthOf(sessions[sessionId].members));
+                if(lengthOf(sessions[sessionId].members) == 0) {
+                    log("Deleting empty session.");
+                    delete sessions[sessionId];          
+                }
+                else if(sessions[sessionId].hostId == connId) {  //so you were the host, need to get a new
+            
+                    log("Host has disconnected. Electing new host...");    
+                    var newHostId = electHost(sessionId);  
+                    log("ID: " + newHostId + " is now the new host.");
+            
+                    log("Informing session members...");
+                    log(lengthOf(sessions[sessionId].members));
+                    for(memberId in sessions[sessionId].members) {
+                    if(sessions[sessionId].hostId != memberId)
+                        sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [newHostId]}));
+                    else
+                        sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [""]}));
+                    }
+            
+                    log("All sessions has been informed.");
+
+                    log("Informing host socket is closed");
+                    sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
+                } else {
+                    log("Informing peers socket is closed");
+                    sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
+                }
+            });
+            
+            /*FOR WHAT IS THIS?!?!?
+            client.on("getIp", function() {
+                client.emit("ip", wSocket.upgradeReq.connection.remoteAddress);   
+                log("IPReq: " + wSocket.upgradeReq.connection.remoteAddress);       
+            });*/
+            
+            //LOG NEW CLIENT INFO
+            log("New client ID: " + client.Username + "@" + client.ConnectionId);
+            
+            //emit SessionJoiner with hostResult and joined sessionId   
+            client.emit("SessionJoined", hostResult, sessionId);           
+        });       
+    });
+});
 
 
-
-
-//EVERYTHING SET, START DB AND LISTEN HTTP AND WS CONNECTIONS
+//------------------------------------  EVERYTHING SET, START DB AND LISTEN HTTP AND WS CONNECTIONS -------------------------------
 log("Initiating Maestro...");
 console.log("");
 log("Initiating IdManager...");
@@ -62,8 +163,6 @@ idManager.Start("mongodb://localhost/test", function(error) {
     if(error)
         throw error.toString();     //if some error while starting idManager, crash application   
     log("IdManager initiated.");
-    
-    idManager.SessionClear();
     
 // start the server on the env port
     log("Starting HTTP and WS Server...");
@@ -75,164 +174,8 @@ idManager.Start("mongodb://localhost/test", function(error) {
 });  
     
 
-   
-    
+//-------------------   Miscelaneous Functions    -------------------//
 
-
-
-
-
-
-
-
-//--------------------------------------- SETUP MADE, NOW WHAT REALLY MATTERS :) -----------------------------------------------
-
-
-//var connections = [];   //array to store connection objects
-
-//var sessions = [];  //array to store session objects
-
-/*
-
-wsServer.on("connection", function(wSocket) {   //websocket connection event handler
-    
-    var client = new Wocket(wSocket);   //inits wocket instance with the just connected websocket
-    
-    client.on("idRequest", function() {
-            
-        
-        
-    });
-    
-    
-    
-    
-    
-    
-    client.on("error", function(error) {   //instance to handle websocket errors
-        log("Error while dealing new websocket: " + error);       
-    });
-    
-    client.on("getIp", function() {
-        client.emit("ip", wSocket.upgradeReq.connection.remoteAddress);   
-        log("IPReq: " + wSocket.upgradeReq.connection.remoteAddress);
-        
-    });
-    
-    
-    
-    
-    
-    var connId = "";
-    var username = "";
-    
-    log("New connection.");
-    
-    
-
-
-    client.on("close", function(code, message) {   //instance to handle websocket errors
-        //verify if an object has been created for this instance
-        //if so, remove it;
-        
-        //got to verify what else should be discarted
-        if(!connId)
-            log("Temp client disconnected.");        
-    });
-
-    client.on("createSession", function() {
-        var newSession = createSession();
-        log("New session created: " + newSession);
-        log(lengthOf(connections) + " " + lengthOf(sessions));
-        client.emit("sessionCreated", newSession);
-    });
-               
-    client.on("joinSession", function(sessionId, user) {
-        //atribuite id only when joining sessions
-        //generate timeout in case information is not send by connected client
-
-        if(!sessions[sessionId]) {           
-            client.emit("joinError", sessionId);
-            log("Session join error");
-            client.close();
-            return;   
-        }
-        
-        //get session ref
-        connSession = sessionId;
-        username = user;
-        //  Create new connection ID    //
-        connId = getId(10);    
-        while(connections[connId])
-        connId = getId(10);        
-        connections[connId] = socket;
-        log("New client ID: " + username + " " + connId);
-        //  -------------------------   //
-        
-        //          Join Session        //
-        sessions[sessionId].members[connId] = socket;
-        
-        if(sessions[sessionId].hostId == "") {
-            sessions[sessionId].hostId = connId;
-            socket.send(getDataStr({type: "sessionJoined",data: ["", sessionId, connId]}));
-        } else {
-            var hostId = sessions[sessionId].hostId;
-            socket.send(getDataStr({type: "sessionJoined",data: [hostId, sessionId, connId]}));
-                    //got to have two separate emites due to in case of host it has to be empty and it is changed in the middle way
-        }
-        
-        socket.on("close", function() {
-            
-            if(sessions[sessionId].members[connId])
-                delete sessions[sessionId].members[connId];
-
-            if(connections[connId])
-                delete connections[connId];
-            
-            log("Client " + connId + " disconnected.");
-            log("Session members: " + lengthOf(sessions[sessionId].members));
-            if(lengthOf(sessions[sessionId].members) == 0) {
-                log("Deleting empty session.");
-                delete sessions[sessionId];          
-            }
-            else if(sessions[sessionId].hostId == connId) {  //so you were the host, need to get a new
-            
-                log("Host has disconnected. Electing new host...");    
-                var newHostId = electHost(sessionId);  
-                log("ID: " + newHostId + " is now the new host.");
-            
-                log("Informing session members...");
-                log(lengthOf(sessions[sessionId].members));
-                for(memberId in sessions[sessionId].members) {
-                if(sessions[sessionId].hostId != memberId)
-                    sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [newHostId]}));
-                else
-                    sessions[sessionId].members[memberId].send(getDataStr({type: "newHost", data: [""]}));
-                }
-            
-                log("All sessions has been informed.");
-
-                log("Informing host socket is closed");
-                sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
-            } else {
-                log("Informing peers socket is closed");
-                sessions[sessionId].members[sessions[sessionId].hostId].send(getDataStr({type: "peerClosure", data: [connId]}));
-            }
-        });
-    });
-    
-    client.on("peerData", function(destId, sessionId, data) {
-        //maybe implement query id in the future
-        try {
-            if(sessions[sessionId].members[destId])
-                sessions[sessionId].members[destId].send(getDataStr({type: "peerData", data: [connId,data]}));
-            else
-                socket.send(getDataStr({type: "peerDataError", data: [destId,data]}));
-        } catch (error) {
-                    
-        }
-    });
-});
 
 function electHost(sessionId) {
     //for now, take the first client connected
@@ -244,9 +187,8 @@ function electHost(sessionId) {
         log("THE NEW HOST ISSS" + sessions[sessionId].hostId);
         return memberId;
     }
-    
-    
-}*/
+
+}
     
 function log(string){   //wrap for log info into the console
     console.log(Misc.GetTimeStamp() + " " + string);
