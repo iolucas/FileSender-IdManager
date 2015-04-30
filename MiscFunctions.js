52766337a5671df6b2026575094628cc228c491f
lucasolivier <lucas.o@live.com>
2015-04-30 01:33:04 -0300

    Added self ref; Added LengthOf

--- MiscFunctions.js
+++ MiscFunctions.js
@@ -4,6 +4,8 @@ module.exports = new Misc();
 
 function Misc() {
     
+    var self = this;
+    
     //Generate Random AlphaNumeric Id with the desired size
     this.GetAlphaNumId = function(size) {   
         var text = "";
@@ -11,7 +13,7 @@ function Misc() {
         for( var i=0; i < size; i++ )
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
-    }
+    };
     
     //Generate Random Numeric Id with the desired size
     this.GetNumId = function(size) {   
@@ -20,7 +22,7 @@ function Misc() {
         for( var i=0; i < size; i++ )
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
-    }   
+    }  ; 
     
     //Generate Random Alphabetic Id with the desired size
     this.GetAlphaNumId = function(size) {   
@@ -29,12 +31,26 @@ function Misc() {
         for( var i=0; i < size; i++ )
             text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
-    }
+    };
     
-    //Return a text with the current timestamp
+    //Get a real length of a non standard array
+    this.LengthOf = function(obj) {
+        var c=0;
+        for(var fieldName in obj)
+            c++;
+        return c;
+    };
+    
+    /*this.log = function(string){   //wrap for log info into the console
+        console.log(self.GetTimeStamp() + " " + string);
+    }*/
+    
+        //Return a text with the current timestamp
     this.GetTimeStamp = function() {
         var cTime = new Date();
             //return (cTime.getHours()>=10?cTime.getHours():"0"+cTime.getHours()) + ":" + cTime.getMinutes() + ":" + cTime.getSeconds();
         return (cTime.getHours()>=10?cTime.getHours():"0"+cTime.getHours()) + ":" + (cTime.getMinutes()>=10?cTime.getMinutes():"0"+cTime.getMinutes()) + ":" + (cTime.getSeconds()>=10?cTime.getSeconds():"0"+cTime.getSeconds());   
-    }
+    };
 }
+    
+
